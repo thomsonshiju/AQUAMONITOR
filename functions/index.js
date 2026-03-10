03994a0f70b58c0fef8c4803c45835aa0fbb3cb3
thomsonshiju <thomsonshiju11@gmail.com>
2026-03-10 16:04:14 +0530

    Fix port for Railway

--- functions/index.js
+++ functions/index.js
@@ -45,7 +45,7 @@ app.post("/sendEmail", async (req, res) => {
 });
 
 // IMPORTANT for Railway
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
